582e0a313526c9ff44565e9c89c76aeabc1d8424
JohnOiko <72659858+JohnOiko@users.noreply.github.com>
2024-04-05 14:30:26 +0300

    Moved training from main script to ResNet50.py. Added training function to ResNet50.py with training and validation loss calculation as well as a tqdm progress bar.

--- src/models/ResNet50.py
+++ src/models/ResNet50.py
@@ -1,1 +1,47 @@
-#def train()
+from tqdm import tqdm
+import torch
+
+
+def train(model, device, train_loader, optimizer, loss_function, gradient_accumulation_steps, epochs, val_loader=None):
+    for epoch in range(1, epochs + 1):
+        with tqdm(train_loader, unit="batch") as pbar:
+            # Set training mode
+            model.train()
+            for batch_idx, (data, target) in enumerate(pbar):
+                pbar.set_description(f"Epoch {epoch}")
+                data, target = data.to(device), target.to(device)
+
+                # Compute loss and perform backpropagation
+                output = model(data)
+                train_loss = loss_function(output, target)
+                train_loss.backward()
+
+                # Optimize and reset accumulated gradients after gradient_accumulation_steps batches
+                if batch_idx % gradient_accumulation_steps == 0:
+                    optimizer.step()
+                    optimizer.zero_grad()
+
+                # Evaluation
+                if batch_idx == len(pbar) - 1 and val_loader is not None:
+                    # Set evalutaion mode
+                    model.eval()
+                    total = 0
+                    running_loss = 0.0
+                    with torch.no_grad():
+                        for val_batch_idx, (data, target) in enumerate(val_loader):
+                            data, target = data.to(device), target.to(device)
+
+                            output = model(data)
+                            val_loss = loss_function(output, target)
+                            total += target.size(0)
+                            running_loss = running_loss + val_loss.item()
+                        mean_val_loss = running_loss / val_batch_idx
+
+                    # Update progress bar with validation loss
+                    pbar.set_postfix(loss=train_loss.item(), val_loss=mean_val_loss)
+
+                else:
+                    # Update progress bar with train loss
+                    pbar.set_postfix(train_loss=train_loss.item())
+
+    return model
--- src/simple_domain.py
+++ src/simple_domain.py
@@ -3,22 +3,23 @@ from src.utils import utils
 from torchvision import models, transforms
 from torch.utils.data import DataLoader
 import argparse
-import time
 from torch import nn
 import torch
-from torch.autograd import profiler
+from src.models import ResNet50
 
 
-def main():
-    # Command line arguments setup
+def parse_args():
     parser = argparse.ArgumentParser(prog="Simple Domain")
     parser.add_argument('-d', '--device_id', type=int,
                         help='the id of the device to be used for hardware acceleration if available')
     parser.add_argument('-nhw', '--no_hw_accel', action="store_true",
                         help='disable hardware acceleration by running on the cpu')
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    # Device creation based on the available hardware acceleration
+
+def main():
+    args = parse_args()
+
     device = utils.select_device(args.device_id, args.no_hw_accel)
 
     # Dataset setup
@@ -28,13 +29,14 @@ def main():
     mnist_test = torchvision.datasets.MNIST(root=dataset_root, train=False, download=True, transform=transform)
     num_classes = len(mnist_train.classes)
 
-
-    batch_size = 32
+    # Params
+    batch_size = 1024
     epochs = 50
     learning_rate = 0.001
     momentum = 0.9
     weight_decay = 0.00001
-    trace = False
+    gradient_accumulation_steps = 1
+    log_interval = 5
 
     train_kwargs = {'batch_size': batch_size}
     test_kwargs = {'batch_size': batch_size}
@@ -44,80 +46,16 @@ def main():
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
 
-    train_loader = DataLoader(mnist_train, **train_kwargs)
+    train_subset, val_subset = torch.utils.data.random_split(mnist_train, [50000, 10000], generator=torch.Generator().manual_seed(1))
+    train_loader = DataLoader(dataset=train_subset, **train_kwargs)
+    val_loader = DataLoader(dataset=val_subset, **train_kwargs)
     test_loader = DataLoader(mnist_test, **test_kwargs)
 
-    print(train_loader)
-
-    model = models.resnet50(num_classes=num_classes).to(device)
-    ci_train = False
-
-
-
     # Load the model on the device
-    start = time.time()
-
-    print('Finished moving {} to device: {} in {}s.'.format('ResNet50', device, time.time() - start))
-
-    cross_entropy_loss = nn.CrossEntropyLoss().to(device)
-
-    highest_accuracy = 0
-
-    for t in range(epochs):
-        print(f"Epoch {t + 1}\n-------------------------------")
-
-        size = len(train_loader.dataset)
-
-        # Train
-        optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum, weight_decay=weight_decay)
-
-        optimize_after_batches = 1
-        start = time.time()
-        for batch, (X, y) in enumerate(train_loader):
-            X = X.to(device)
-            y = y.to(device)
-
-            if (trace):
-                with profiler.profile(record_shapes=True, with_stack=True, profile_memory=True) as prof:
-                    with profiler.record_function("model_inference"):
-                        # Compute loss and perform backpropagation
-                        pred = model(X)
-
-                        batch_loss = cross_entropy_loss(pred, y)
-                        batch_loss.backward()
-
-                        if batch % optimize_after_batches == 0:
-                            optimizer.step()
-                            optimizer.zero_grad()
-                print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=1000))
-                break
-            else:
-                # Compute loss and perform backpropagation
-                pred = model(X)
-                batch_loss = cross_entropy_loss(model(X), y)
-                batch_loss.backward()
-
-                if batch % optimize_after_batches == 0:
-                    optimizer.step()
-                    optimizer.zero_grad()
-
-            if (batch + 1) % 100 == 0:
-                batch_loss_cpu, current = batch_loss.to('cpu'), (batch + 1) * len(X)
-                print(f"loss: {batch_loss_cpu.item():>7f}  [{current:>5d}/{size:>5d}] in {time.time() - start:>5f}s")
-                start = time.time()
-
-            if ci_train:
-                print(f"train [{len(X):>5d}/{size:>5d}] in {time.time() - start:>5f}s")
-                break
-
-    print("Done! with highest_accuracy: ", highest_accuracy)
-
-
-
-
-
-
-
+    model = models.resnet50(num_classes=num_classes).to(device)
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum, weight_decay=weight_decay)
+    loss_function = nn.CrossEntropyLoss().to(device)
+    ResNet50.train(model, device, train_loader, optimizer, loss_function, gradient_accumulation_steps, epochs, val_loader=val_loader)
 
 
 if __name__ == '__main__':
